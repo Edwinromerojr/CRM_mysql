5a13cd0fccac2672031de6447ddbfa09b59755c5
Edwin Romero <edwinrmrjr@gmail.com>
2023-11-02 08:49:41 +0800

    Configure mysql

--- dcrm/dcrm/settings.py
+++ dcrm/dcrm/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "website",
 ]
 
 MIDDLEWARE = [
@@ -74,9 +75,13 @@ WSGI_APPLICATION = "dcrm.wsgi.application"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'mysql1',
+        'USER': 'root',
+        'PASSWORD': 'testpass123',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
